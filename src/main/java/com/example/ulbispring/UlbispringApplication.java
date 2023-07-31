b3174f56305050d049a079a12e90c5a08c5ecbb3
Ira Stefan <ira.stefan91@gmail.com>
2023-07-31 09:45:44 +0300

    init  commit

---
+++ src/main/java/com/example/ulbispring/UlbispringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ulbispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlbispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlbispringApplication.class, args);
+	}
+
+}
